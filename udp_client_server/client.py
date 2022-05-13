db06531b2c4a49d57ca03484b60174aeeadcbc44
Dmitry <dmitry.kharisov@gmail.com>
2022-05-14 01:04:56 +0700

    update client

--- udp_client_server/client.py
+++ udp_client_server/client.py
@@ -3,6 +3,7 @@ import struct
 import random
 import argparse
 from pathlib import Path
+from random import randint
 
 #external depends
 import crc32c
@@ -10,12 +11,11 @@ import crc32c
 Port = 9000
 IP = "127.0.0.1"
 
+def randomID():
+    return (randint(1, 99999999)).to_bytes(8,byteorder='big')
+
 def client(filepath):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
-
-    hello = bytes("Hello!", 'utf-8')
-    data = struct.pack('<llB8s1455s', 3, 2, 0, b'00000001', hello)
-
     chunks=[]
     filename = filepath
     chunksize = 1455
@@ -32,28 +32,27 @@ def client(filepath):
             else:
                 break  
 
-    print("file crc = " + str(crc_whole_file))
     random.shuffle(chunks)
-    print(len(chunks))
     total_packet = len(chunks)
-    crc = 0
+    
     already_sent = 0
+    id = randomID()
+    PUT = 1
     for temp_chunk in chunks:
         chunk_data = temp_chunk[0]
         chunk_number = temp_chunk[1]
-        crc = 0
-        crc = crc32c.crc32c(chunk_data, crc)
+        crc = crc32c.crc32c(chunk_data, 0)
 
-        data = struct.pack(f'<llB8s{len(chunk_data)}s', chunk_number, total_packet, 1, (1).to_bytes(8,byteorder='big'), chunk_data)
+        data = struct.pack(f'<llB8s{len(chunk_data)}s', chunk_number, total_packet, PUT, id, chunk_data)
 
         s.sendto(data, (IP, Port))
         already_sent += 1
-        # print("\n\n 1. Client Sent : ", send_data, "\n\n")
         data, address = s.recvfrom(4096)
-        # 4+4+1+8+4
+
         seq_number, seq_total, type, id, crc_from_server = list(struct.unpack('<llB8s4s', data))
         crc_from_server = int.from_bytes(crc_from_server,"little")
         if(seq_total != total_packet):
+            pass
             print('crc32 from client = {:#010x}'.format(crc))
             print('crc32 from server = {:#010x}'.format(crc_from_server))
             print(f"\nsent {seq_total} from {total_packet}\npacket number is {chunk_number}")
