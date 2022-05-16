9f45bb6836c5d19f5ddc0ea5d5b1478263a67d5a
Dmitry <dmitry.kharisov@gmail.com>
2022-05-17 01:52:35 +0700

    kill all processes after demonstration

--- udp_client_server/run.sh
+++ udp_client_server/run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-./build_linux/udp_server & python3 client.py $1 #& python3 client.py $1 
+./build_linux/udp_server &
+export APP_PID=$!
+
+( python3 client.py $1 &
+python3 client.py $1 &
+python3 client.py $1 &
+python3 client.py $1 &
+wait )
+
+kill $APP_PID
+#python3 client.py $1 & python3 client.py $1 & python3 client.py $1 & python3 client.py $1 
