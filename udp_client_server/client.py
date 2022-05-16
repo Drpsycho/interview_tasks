46a769cb6a8384ad644cabd782caacf0024ba284
Dmitry <dmitry.kharisov@gmail.com>
2022-05-17 01:51:27 +0700

    resend the packet if the checksum mismatched

--- udp_client_server/client.py
+++ udp_client_server/client.py
@@ -30,41 +30,48 @@ def readfile_and_get_checksum(filename):
                 break  
     return chunks, crc_whole_file
 
+def send_package(client_socket, chunk_data, chunk_number, id, total_packet) :
+    PUT = 1
+    data = struct.pack(f'<llB8s{len(chunk_data)}s', chunk_number, total_packet, PUT, id, chunk_data)
+    return client_socket.sendto(data, (IP, Port)) == len(data) 
+
+def recive_package(client_socket):    
+    data, address = client_socket.recvfrom(4096)
+    seq_number, seq_total, type, id, crc_from_server = list(struct.unpack('<llB8s4s', data))
+    crc_from_server = int.from_bytes(crc_from_server,"little")
+    return seq_total, crc_from_server
+
 def client(path_to_file):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)    
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)    
     chunks, crc_whole_file = readfile_and_get_checksum(path_to_file)
     random.shuffle(chunks)
     total_packet = len(chunks)
     
     already_sent = 0
     id = randomID()
-    PUT = 1
-    for temp_chunk in chunks:
-        chunk_data = temp_chunk[0]
-        chunk_number = temp_chunk[1]
+    
+    for chunk_data, chunk_number  in chunks:
         crc = crc32c.crc32c(chunk_data, 0)
 
-        data = struct.pack(f'<llB8s{len(chunk_data)}s', chunk_number, total_packet, PUT, id, chunk_data)
-
-        s.sendto(data, (IP, Port))
+        while(True):
+            send_package(client_socket, chunk_data, chunk_number, id, total_packet)
+            seq_total, crc_from_server = recive_package(client_socket)
+            if crc == crc_from_server:
+                # print('crc32 from client = {:#010x}'.format(crc))
+                # print('crc32 from server = {:#010x}'.format(crc_from_server))
+                # print(f"\nsent {seq_total} from {total_packet}\npacket number is {chunk_number}")
+                break
+            if total_packet == seq_total:
+                print(f"\nDone, sent {seq_total} packets!\nThe checksum of the entire file is")
+                print('crc32 from client = {:#010x}'.format(crc_whole_file))
+                print('crc32 from server = {:#010x}'.format(crc_from_server))
+                break
         already_sent += 1
-        data, address = s.recvfrom(4096)
-
-        seq_number, seq_total, type, id, crc_from_server = list(struct.unpack('<llB8s4s', data))
-        crc_from_server = int.from_bytes(crc_from_server,"little")
-        if(seq_total != total_packet):
-            print('crc32 from client = {:#010x}'.format(crc))
-            print('crc32 from server = {:#010x}'.format(crc_from_server))
-            print(f"\nsent {seq_total} from {total_packet}\npacket number is {chunk_number}")
-        else:
-            print(f"\nDone, sent {seq_total} packets!\nThe checksum of the entire file is")
-            print('crc32 from client = {:#010x}'.format(crc_whole_file))
-            print('crc32 from server = {:#010x}'.format(crc_from_server))
-
-    s.close()
+    client_socket.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Client for the UDP server.')
     parser.add_argument("filepath", type=Path)
     p = parser.parse_args()
     client(p.filepath)
+    exit(0)
